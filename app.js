a773f9142eb4ce032f7d6e99103abe3fb0784bc8
paulacruz <cruz.amp12@gmail.com>
2019-05-06 22:02:44 -0700

    added capitol hill and alki

--- app.js
+++ app.js
@@ -162,6 +162,113 @@ seaCenter.render();
 // totalDailySales[hourlySales];
 //total cookies
 
+///////////////////////////////**** */Capitol Hill***////////////////////////
+
+
+
+//source mdn math.random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+
+//store hours Seattle Center
+var storeHours = ['6am', '7am','8am','9am', '10am', '11am', '12pm','1pm', '2pm', '3pm', '4pm', '5pm',
+'6pm','7pm','8pm'];
+
+//var to call elementById
+var capHill = document.getElementById('capHill');
+
+
+// pike obj literal
+var capitolHill = {
+  minCust: 20,
+  maxCust: 38,
+  salesPerCust: 2.3,
+  hourlySales: [],
+  hourlySalesCalc: function(){
+      for (var i = 0; i < storeHours.length; i++){
+      this.hourlySales.push(Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.salesPerCust))
+      } 
+  },
+
+  //render - A component with a render prop takes a function that returns a React element and calls it instead of implementing its own render logic.
+render: function(){
+      for (var i = 0; i < storeHours.length; i++){
+      // create an element
+      var liEl = document.createElement('li');
+      console.log('liEl is', liEl);
+      // give the element content
+      liEl.textContent = storeHours[i] + ': ' + this.hourlySales[i] + ' cookies';        
+      console.log('liEl with content', liEl);
+      // append the element to the dom
+      capHill.appendChild(liEl);
+      }
+    }
+}
+
+capitolHill.hourlySalesCalc();
+capitolHill.render();
+// totalDailySales[hourlySales];
+//total cookies
+
+///////////////////////////////**** */Alki***////////////////////////
+
+
+
+//source mdn math.random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+
+//store hours Seattle Center
+var storeHours = ['6am', '7am','8am','9am', '10am', '11am', '12pm','1pm', '2pm', '3pm', '4pm', '5pm',
+'6pm','7pm','8pm'];
+
+//var to call elementById
+var alkiOne = document.getElementById('alkiOne');
+
+
+// pike obj literal
+var alkiTwo = {
+  minCust: 20,
+  maxCust: 38,
+  salesPerCust: 2.3,
+  hourlySales: [],
+  hourlySalesCalc: function(){
+      for (var i = 0; i < storeHours.length; i++){
+      this.hourlySales.push(Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.salesPerCust))
+      } 
+  },
+
+  //render - A component with a render prop takes a function that returns a React element and calls it instead of implementing its own render logic.
+render: function(){
+      for (var i = 0; i < storeHours.length; i++){
+      // create an element
+      var liEl = document.createElement('li');
+      console.log('liEl is', liEl);
+      // give the element content
+      liEl.textContent = storeHours[i] + ': ' + this.hourlySales[i] + ' cookies';        
+      console.log('liEl with content', liEl);
+      // append the element to the dom
+      alkiOne.appendChild(liEl);
+      }
+    }
+}
+
+alkiTwo.hourlySalesCalc();
+alkiTwo.render();
+// totalDailySales[hourlySales];
+//total cookies
+
+
+
+
 
 
 
